b8adf733ddcde6f7587a1a4e1571ae0159e4aff1
Kolkovskiy Evgeny <evgeny.kolkovski@gmail.com>
2022-01-28 22:22:50 +0300

    fix:fixingcode

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@
 
 module.exports = function towelSort (matrix) {
 
-  let sortArray = [];
-  if (matrix === undefined) {
-    return []
-  } 
-  for (i = 0; i <= matrix.length; i++) {
-    if (i % 2 === 0) {
-      sortArray = sortArray.concat(matrix[i].reverse());
-    } else {
-      sortArray = sortArray.concat(matrix[i]);
-    }
+  let matrixSort = [];
+
+  if(!Array.isArray(matrix) || matrix.length === 0) return [];
+  for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 != 0) {
+        matrixSort = matrixSort.concat(matrix[i].reverse());
+      } else {
+        matrixSort = matrixSort.concat(matrix[i]);
+      }
   }
-  return sortArray;
+
+  return matrixSort;
 }
